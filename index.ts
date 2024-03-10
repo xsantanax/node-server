418ee344320bb80304ad8341ba5dcd404eba5af9
xsantanax <rafaelsantana111@gmail.com>
2024-03-10 18:15:31 -0300

    npm i cors + npm i --save-dev @types/cors + app.use(cors())

--- index.ts
+++ index.ts
@@ -1,7 +1,9 @@
 import express, { Express, Request, Response } from "express"
+import cors from "cors"
 
 const app: Express = express()
 const port: number = 5000
+app.use(cors())
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello World!")
